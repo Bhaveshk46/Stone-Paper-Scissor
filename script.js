978a6d0e01a98dbff1045ecaf68351f42db82422
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-28 10:10:00 +0000

    Remove the logic that plays exactly five rounds

--- script.js
+++ script.js
@@ -1,78 +1,77 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
+function getComputerChoice() {
     const choice = Math.random();
-    if (choice<0.33){
+    if (choice < 0.33) {
         return "stone"
     }
-    else if (choice<0.66){
+    else if (choice < 0.66) {
         return "paper"
     }
-    else
-    {
+    else {
         return "scissor"
     }
 
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     const humanAttempt = prompt("Enter Stone, Paper or Scissor");
-        return humanAttempt.toLowerCase();
+    return humanAttempt.toLowerCase();
 }
 
-function playground(computer,human){
+function playground(computer, human) {
     let humanChoice = human;
     let computerChoice = computer;
-    if (humanChoice === computerChoice){
+    if (humanChoice === computerChoice) {
         console.log("Tie! same choice");
     }
-    else if (humanChoice==="stone"&&computerChoice==="paper"){
+    else if (humanChoice === "stone" && computerChoice === "paper") {
         console.log("You, Loose! paper beats Stone");
         computerScore++;
     }
-    else if (humanChoice==="stone"&&computerChoice==="scissor"){
+    else if (humanChoice === "stone" && computerChoice === "scissor") {
         console.log("You, Won! Stone beats Scissor");
         humanScore++;
     }
-    else if (humanChoice==="paper"&&computerChoice==="stone"){
+    else if (humanChoice === "paper" && computerChoice === "stone") {
         console.log("You, Won! Paper beats Stone");
         humanScore++;
     }
-    else if (humanChoice==="paper"&&computerChoice==="scissor"){
+    else if (humanChoice === "paper" && computerChoice === "scissor") {
         console.log("You, Loose! Scissor beats Paper");
         computerScore++;
     }
-    else if (humanChoice==="scissor"&&computerChoice==="stone"){
+    else if (humanChoice === "scissor" && computerChoice === "stone") {
         console.log("You, Loose! Stone beats Scissor");
         computerScore++;
     }
-    else if (humanChoice==="scissor"&&computerChoice==="paper"){
+    else if (humanChoice === "scissor" && computerChoice === "paper") {
         console.log("You, Won! Scissor beats Paper");
         humanScore++;
     }
-    else{
+    else {
         console.log("Error!");
     }
 
 
 }
 
-function playGame(){
-for(i =1;i<=5;i++){
+function playGame() {
+
     const computer = getComputerChoice();
     const human = getHumanChoice();
-    playground(computer,human);
-}
-if(humanScore>computerScore){
-    console.log("Congratulations! You won the game!!!")
-}
-else if(humanScore<computerScore){
-    console.log("Ufff! Better luck next time")
-}
-else{
-    console.log("Tie!")
-}
+    playground(computer, human);
+
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game!!!")
+    }
+    else if (humanScore < computerScore) {
+        console.log("Ufff! Better luck next time")
+    }
+    else {
+        console.log("Tie!")
+    }
 }
 
 playGame()
